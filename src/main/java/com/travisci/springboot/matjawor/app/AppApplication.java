224daa331fe78976d1c5f81d3acafd4668f24b2d
Mateusz Jaworski <jawor.info@gmail.com>
2021-10-19 21:56:45 +0200

    import missing packages

--- src/main/java/com/travisci/springboot/matjawor/app/AppApplication.java
+++ src/main/java/com/travisci/springboot/matjawor/app/AppApplication.java
@@ -1,7 +1,9 @@
 package com.travisci.springboot.matjawor.app;
 
 import java.util.Arrays;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,7 +12,7 @@ import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class AppApplication {
-	private static Logger LOG = LoggerFactory.getLogger(SpringBootConsoleApplication.class);
+	private static Logger LOG = LoggerFactory.getLogger(AppApplication.class);
 
 
 	public static void main(String[] args) {
