156d53d186ac8ed412107683266f22b83deeba1e
Mateusz Jaworski <jawor.info@gmail.com>
2021-10-20 13:47:19 +0200

    fix

--- src/main/java/com/travisci/springboot/matjawor/app/AppApplication.java
+++ src/main/java/com/travisci/springboot/matjawor/app/AppApplication.java
@@ -58,6 +58,7 @@ class MyCustomRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("MESSAGE FROM DEV: " + message);
+		System.out.println("AFTER SUCCESFULLY RUN APP WILL BE CLOSE);
 		context.close();
 	}
 }
