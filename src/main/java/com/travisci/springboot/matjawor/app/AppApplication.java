1521d904b34cd3ed1917e046e53622d13f21132a
Mateusz Jaworski <72973825+matjawor@users.noreply.github.com>
2021-10-20 14:27:11 +0200

    travis ci config spring profiles custom runner (#1)
    * add stages and filter branch
    
    * replace master with main
    
    * extend class behaviour and change .travis
    
    * import missing packages
    
    * fix import syntax error
    
    * fix healthcheck
    
    * fix bash operator
    
    * add dev profile and handling
    
    * fix add profile
    
    * fix syntax
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix typo

--- src/main/java/com/travisci/springboot/matjawor/app/AppApplication.java
+++ src/main/java/com/travisci/springboot/matjawor/app/AppApplication.java
@@ -1,13 +1,64 @@
 package com.travisci.springboot.matjawor.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class AppApplication {
+	private static Logger LOG = LoggerFactory.getLogger(AppApplication.class);
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		LOG.info("STARTING MY APP");
+		ConfigurableApplicationContext ctx = SpringApplication.run(AppApplication.class, args);
+		LOG.info("APPLICATION FINISHED");
 	}
 
+	@Bean
+    @Profile(value="prod")
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+            System.out.println("IN PRODUCTION");
+            System.out.println("MESSAGE FROM PROD PROPERTIES");
+
+			System.out.println("Beans list provided by Springboot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+}
+
+@Component
+@Profile(value="dev")
+class MyCustomRunner implements CommandLineRunner {
+	@Autowired
+	private ConfigurableApplicationContext context;
+
+	@Value("${message}")
+	private String message;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("MESSAGE FROM DEV: " + message);
+		System.out.println("AFTER SUCCESSFULLY RUN APP WILL BE CLOSE");
+		context.close();
+	}
 }
