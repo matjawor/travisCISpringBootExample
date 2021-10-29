f2cadf0153dad6b9648e7f132ebbc35511c6ac91
Mateusz Jaworski <jawor.info@gmail.com>
2021-10-29 10:52:17 +0200

    add rest endpoint

--- src/main/java/com/travisci/springboot/matjawor/app/HelloController.java
+++ src/main/java/com/travisci/springboot/matjawor/app/HelloController.java
@@ -1,5 +1,4 @@
-package com.example.springboot;
-
+package com.travisci.springboot.matjawor.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "HELLO WORLD!";
+		return "HELLO FROM APP";
 	}
 
 }
